78f291dcc03ef391f399a2be1c4c96ebd057c76d
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-11-26 19:57:02 +0500

    Task4

--- Tyuiu.AnishchenkoVA.Sprint4.Task4.V19.Test/DataServiceTest.cs
+++ Tyuiu.AnishchenkoVA.Sprint4.Task4.V19.Test/DataServiceTest.cs
@@ -1,6 +1,5 @@
-using Tyuiu.AnishchenkoVA.Sprint4.Task3.V11.Lib;
-
-namespace Tyuiu.AnishchenkoVA.Sprint4.Task3.V11.Test
+using Tyuiu.AnishchenkoVA.Sprint4.Task4.V19.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint4.Task4.V19.Test
 {
     [TestClass]
     public class DataServiceTest
@@ -11,7 +10,7 @@ namespace Tyuiu.AnishchenkoVA.Sprint4.Task3.V11.Test
             DataService ds = new DataService();
             int[,] a = { { 8, 6, 9, 4, 5 }, { 9, 4, 8, 5, 6 }, { 9, 7, 9, 8, 4 }, { 4, 6, 5, 7, 8 }, { 6, 6, 7, 6, 4 } };
             int res = ds.Calculate(a);
-            int kk = 10;
+            int kk = 72;
             Assert.AreEqual(kk, res);
         }
     }
---
+++ Tyuiu.AnishchenkoVA.Sprint4.Task4.V19/Program.cs
@@ -0,0 +1,62 @@
+﻿using Tyuiu.AnishchenkoVA.Sprint4.Task4.V19.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint4.Task4.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Анищенко В. А. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Анищенко Виктор Александрович | ИИПБ-24-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный значениями с клавиатуры в диапазоне от 1 до 7.              *");
+            Console.WriteLine("* Найдите сумму нечетных элементов массива.                               *");
+            Console.WriteLine("* {6,7,3,5,1} , {6,5,2,2,7} , {2,6,4,6,6} , {7,5,5,2,6} , {3,1,5,7,7}     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в матрице: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в матрице: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] x = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Введите {i},{j} элемент матрицы: ");
+                    x[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМатрица: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0;j < cols; j++)
+                {
+                    Console.Write($"{x[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма нечетных элементов матрицы = " + ds.Calculate(x));
+            Console.ReadKey();
+        }
+    }
+}
