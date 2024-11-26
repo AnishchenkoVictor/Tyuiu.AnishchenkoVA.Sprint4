9ea96aa49ee07dc5cf99f6f00a741e648a01acb5
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-11-26 20:40:48 +0500

    Task6

---
+++ Tyuiu.AnishchenkoVA.Sprint4.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.AnishchenkoVA.Sprint4.Task6.V3.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint4.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var a = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            int res = ds.Calculate(a);
+            int x = 4;
+            Assert.AreEqual(x, res);
+        }
+    }
+}
